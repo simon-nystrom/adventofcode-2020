4ed7987a5209b5d957da02f43d1219340f5c96aa
Simon Nyström <simon.nystrom@svt.se>
2020-12-03 05:59:18 +0100

    updated template

--- utils/template.py
+++ utils/template.py
@@ -1,12 +1,16 @@
 import sys
 from utils.log import log
 
-puzzle_input = sys.stdin.read()
+puzzle_input = [int(x) for x in sys.stdin.read().split('\n')]
 
 @log
-def a():
+def a(puzzle_input):
     pass
 
 @log
-def b():
+def b(puzzle_input):
     pass
+
+
+a(puzzle_input)
+b(puzzle_input)
